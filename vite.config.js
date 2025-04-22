165a1c6be3c0d51bdcaae656210aeb53f59c0aba
Atul <atul.pandey@innovapptive.com>
2025-04-23 01:23:44 +0530

    allowed host

--- vite.config.js
+++ vite.config.js
@@ -7,5 +7,6 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: process.env.PORT || 3000,
+    allowedHosts: ['annu-projects-survey-tool.onrender.com', 'localhost'],
   },
 })
