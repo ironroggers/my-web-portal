c53e028a6c2e7d34dbb4a0bf8dce0e2db8b5635d
Atul <atul.pandey@innovapptive.com>
2025-05-11 05:38:30 +0530

    env

---
+++ src/API/api-keys.jsx
@@ -0,0 +1,9 @@
+// export const AUTH_URL = 'https://auth-api-xz1q.onrender.com';
+// export const SURVEY_URL = 'https://survey-service-nxvj.onrender.com';
+// export const LOCATION_URL = 'https://location-service-mig8.onrender.com';
+// export const ATTENDANCE_URL = 'https://surveytoolbackend.onrender.com';
+
+export const AUTH_URL = 'https://api.annuprojects.com';
+export const SURVEY_URL = 'https://survey.annuprojects.com';
+export const LOCATION_URL = 'https://location.annuprojects.com';
+export const ATTENDANCE_URL = 'https://attendance.annuprojects.com';
