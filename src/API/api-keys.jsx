5c1255c7215506f916b4480de125837e928357d9
akshay yadav <akshayhat@gmail.com>
2025-08-16 16:38:04 +0530

    Update LOCATION_URL in api-keys.jsx to use the production URL for location services.

--- src/API/api-keys.jsx
+++ src/API/api-keys.jsx
@@ -6,7 +6,7 @@
 
 export const AUTH_URL = "https://api.annuprojects.com";
 export const SURVEY_URL = "https://survey.annuprojects.com";
-export const LOCATION_URL = "http://localhost:3002";
+export const LOCATION_URL = "https://location.annuprojects.com";
 export const ATTENDANCE_URL = "https://attendance.annuprojects.com";
 export const HOTO_URL = "https://hoto.annuprojects.com";
 export const GOOGLE_MAPS_DIRECTIONS_API = "AIzaSyC2pds2TL5_lGUM-7Y1CFiGq8Wrn0oULr0";
